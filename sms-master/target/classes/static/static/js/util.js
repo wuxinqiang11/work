943efb51d05c305777b2e6702e69b774a34bf88a
wuxinqiang <wuxinqiang@xii.com>
2022-06-24 02:33:11 -0400

    delete file

--- sms-master/target/classes/static/static/js/util.js
+++
@@ -1,9 +0,0 @@
-//获取当前路径的域名
-var obj = document.getElementById('url');
-var obj1 = document.getElementById('url1');
-var oldvalue = obj.innerHTML;
-var oldvalue1 = obj1.innerHTML;
-
-var url = window.location.href;
-obj.innerHTML = url.substring(0,url.indexOf("carb2b/"))+ "carb2b" +oldvalue;
-obj1.innerHTML = url.substring(0,url.indexOf("carb2b/"))+ "carb2b" +oldvalue1;
